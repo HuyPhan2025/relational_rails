bda726666ebddd854fdb768d742a6b9b672e6c9f
Huy Phan <huyphan520@gmail.com>
2023-02-04 22:33:18 -0600

    Create new spec, controllers and view.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,10 +1,23 @@
 class SchoolsController < ApplicationController
   def index
     @schools = School.order(:created_at)
+    @schools = School.all
   end
 
   def show
     @school = School.find(params[:id])
     @student_count = @school.count_of_students
+  end
+
+  def new
+  end
+
+  def create
+    school = School.create!(school_params)
+    redirect_to "/schools" 
+  end
+
+  def school_params
+    params.permit(:name, :esl_program, :tuition)
   end
 end
